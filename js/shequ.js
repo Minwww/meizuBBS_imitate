2cc1728d7746111f871ffef73a4c75902c0b1cfd
Minwww <mw0520@qq.com>
2016-08-21 14:56:08 +0800

    仿魅族社区

---
+++ js/shequ.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    //登录窗口鼠标滑过效果
+	var timerLogin = null;
+	$('.login-box').hover(function(){
+		$('.login-box s').show().siblings('.not-login-sub').show();
+
+	},function(){
+		timer = setTimeout(function(){
+			$('.login-box s').hide().siblings('.not-login-sub').hide();
+		},500)
+	});
+
+	//鼠标滑过产品显示
+	$('.header-product').hover(function(){	
+		var num = $(this).index();
+		$('.sub-wrap').eq(num-1).stop().slideDown(500).siblings().hide();
+		},function(){
+			$('.sub-wrap').hide();
+	});
+
+	//轮播图
+
+	var num = 0;
+	var liWidth = $('.roll-img li').width();
+	var timer = null;
+
+
+	function auto(){ //自动轮播
+		num++;
+		if(num == 3)num = 0;
+		$('.shequ-roll').animate({
+			marginLeft: '-' + (liWidth + 28)*num +'px', 
+		},500);
+		$('.roll_title a').eq(num).fadeIn(0).siblings().fadeOut(0);
+		$('.roll_small_pic img').eq(num).css('opacity','1').siblings().css('opacity','0.2');
+	}
+	auto();
+
+	timer = setInterval(auto,6000); //定时效果
+
+
+	$('.shequ-roll').hover(function(){
+		$(this).find('.roll_opacity').show();
+		$(this).find('.roll_small').show();
+	},function(){
+		$(this).find('.roll_opacity').hide();
+		$(this).find('.roll_small').hide();
+	},function(){
+	});
+
+	
+
+
+
+
+
+
+
+
+//切换
+$('.firmware_tabs .tabs_title li').mouseover(function(){
+	$(this).addClass('cur').siblings().removeClass('cur');
+	$('.firmware_tabs .tabs_info li').eq($(this).index()).show().siblings().hide();
+})
+
+$('.contact .tabs_title li').mouseover(function(){
+	$(this).addClass('cur').siblings().removeClass('cur');
+	$('.contact .tabs_info li').eq($(this).index()).show().siblings().hide();
+})
+
+
+})
